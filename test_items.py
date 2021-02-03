f80768cb1bd9432eea9bf90277ce72a1c5dd0b44
jmyh <dmitriy.kozlov.97@mail.ru>
2021-02-03 21:04:11 +0300

    add error message

--- test_items.py
+++ test_items.py
@@ -6,5 +6,5 @@ link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 def test_check_add_to_basket_button(browser):
     browser.get(link)
     button = browser.find_element_by_css_selector("button.btn-add-to-basket")
-    assert button is not None
+    assert button is not None, "Button was not found!"
     time.sleep(30)
