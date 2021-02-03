eedc71d96090c4ec12b401f37d931378ca0e5ce2
jmyh <dmitriy.kozlov.97@mail.ru>
2021-02-03 20:58:57 +0300

    Initial commit

---
+++ test_items.py
@@ -0,0 +1,10 @@
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_check_add_to_basket_button(browser):
+    browser.get(link)
+    button = browser.find_element_by_css_selector("button.btn-add-to-basket")
+    assert button is not None
+    time.sleep(30)
